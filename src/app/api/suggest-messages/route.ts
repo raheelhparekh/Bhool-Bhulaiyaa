96eb6d1a7d124acaeb44f3b5565b0966af5e7c99
raheelhparekh <rhparekh2003@gmail.com>
2025-01-17 19:03:37 +0530

    minor edits

--- src/app/api/suggest-messages/route.ts
+++ src/app/api/suggest-messages/route.ts
@@ -10,8 +10,10 @@ export async function POST() {
       "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
 
     const result = await model.generateContent(prompt);
-    console.log(result.response.text());
-    return NextResponse.json({ text: result.response.text() });
+
+    const generatedText= result.response.text();
+    // console.log(result.response.text());
+    return NextResponse.json({ text: generatedText });
   } catch (error) {
     console.error("An unexpected error occurred:", error);
     throw error;
